d616e27fe4571067086562fb4de52c6b3309cf13
jefersonandradde <jefersonandrade@outlook.com>
2015-03-28 19:23:07 -0300

    Último teste do comando diff

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,15 @@
 print("Hello World")
 
-for i in range(10):
-	print(i)
+def imprime_espacos(qtde_espacos):
+	for espacos in range(qtde_espacos):
+		print("")
+
+def imprime_hello_world(qtde_vezes):
+	for vez in range(qtde_vezes):
+		print("Hello World")
+
+
+if __name__ == "__main__":
+	imprime_espacos(10)
+	imprime_hello_world(10)
+	
